3a949b5acdc4fd3ba45044d372fe072d62520bd4
Lassie <3valdas.l@gmail.com>
2019-05-07 14:46:52 +0200

    Footer update?

--- frontend/src/components/NavbarFixedTop.vue
+++ frontend/src/components/NavbarFixedTop.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="nav">
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-            <a class="navbar-brand" href="#">Bio-Trio</a>
+            <a class="navbar-brand" href="#"><img src="" style="height: 20%"></a>
 
             <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
                 <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
