58e8bbdff3d79444ed496eeaebb74252c12c3126
Max Campbell <maxc@maxc.in>
2019-05-07 12:32:36 +0200

    Added backend login

--- backend/src/main/java/com/biotrio/backend/controller/FilmController.java
+++ backend/src/main/java/com/biotrio/backend/controller/FilmController.java
@@ -9,35 +9,36 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
 
     @Autowired
     private FilmRepository filmRepository;
 
-    @GetMapping("/films")
+    @GetMapping("/")
     public List<Film> films(){
         List<Film> filmList = (List<Film>) filmRepository.findAll();
         return filmList;
     }
 
-    @PostMapping("/films")
+    @PostMapping("/")
     public Film filmAdd(@RequestBody Film film) {
         filmRepository.save(film);
         return film;
     }
 
-    @RequestMapping(value = "/films/{index}/delete")
+    @RequestMapping(value = "/{index}/delete")
     public int film(@PathVariable int index) {
         filmRepository.deleteById(index);
         return index;
     }
 
-    @RequestMapping(value = "/films/{index}")
+    @RequestMapping(value = "/{index}")
     public Film filmDelete(@PathVariable int index) {
         return filmRepository.findById(index).get();
     }
 
-    @RequestMapping(value="/films/{index}/edit", method = RequestMethod.POST)
+    @RequestMapping(value="/{index}/edit", method = RequestMethod.POST)
     public Film filmEdit(@PathVariable int index, @RequestBody Film film) {
         film.setId(index);
         filmRepository.save(film);
--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -3,15 +3,15 @@
          aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                <form>
+                <form  v-on:submit.prevent="login">
                     <div class="form-group">
                         <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        <input type="email" v-model="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        <input type="password" v-model="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
@@ -21,10 +21,25 @@
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
         name: "Login",
         props: {
             msg: String
+        },
+        data(){
+            return {
+                username:'',
+                password:''
+            }
+        },
+        methods:{
+            login(){
+                axios.post("http://localhost:8080/user/login",{
+                    username:this.username,
+                    password:this.password
+                })
+            }
         }
     };
 </script>
