5d981d5dfcf3b1ea3f9b8a760dcc4bd390bf99ed
Max Campbell <maxc@maxc.in>
2019-05-07 18:07:54 +0200

    Added session config

--- backend/src/main/java/com/biotrio/backend/controller/UserController.java
+++ backend/src/main/java/com/biotrio/backend/controller/UserController.java
@@ -3,13 +3,15 @@ package com.biotrio.backend.controller;
 import com.biotrio.backend.model.UserCreds;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
 @RequestMapping("/user")
 @CrossOrigin(origins = "*")
 public class UserController {
     @PostMapping("/login")
-    public UserCreds greeting(@RequestBody UserCreds userCreds) {
+    public HttpSession greeting(@RequestBody UserCreds userCreds, HttpSession session) {
         System.out.println(userCreds.getUsername()+userCreds.getPassword());
-        return userCreds;
+        return session;
     }
 }
