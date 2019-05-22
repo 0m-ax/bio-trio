acaba64c9cd863e2009888b47e6f62b0165ec24e
NewGuyAlbert <albe9220@stud.kea.dk>
2019-05-22 22:13:53 +0200

    admin pages under construction

---
+++ frontend/src/views/admin/Movie/Movies.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="Movie">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Length</th>
+                <th scope="col">Genre</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr v-for="movie in items" v-bind:key="movie.id">
+                    <th scope="row">{{movie.movieID}}</th>
+                    <td>{{movie.name}}</td>
+                    <td>{{movie.length}} minutes</td>
+                    <td>{{movie.genre}}</td>
+                    <td><router-link :to="{ name: 'admin-movie', params: { movieID:movie.movieID }}">Editto</router-link></td>
+                    <td><a href="">Deletto</a> </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+
+    import client from "../../../api.js"
+    export default {
+
+        methods:{
+            async loadData(){
+                let resp = await client.get("/movies");
+                this.items = resp.data._embedded.movies
+                this.loading=false;
+            }
+        },
+        created(){
+          this.loadData()
+        },
+        data(){
+            return {
+                openMovie:null,
+                loading:true,
+                items:[
+
+                ]
+            }
+        },
+        name: "Films"
+    };
+</script>
