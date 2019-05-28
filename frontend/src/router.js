dee4759f5225d3ccc027b86c0b9af61cca8553d5
NewGuyAlbert <albe9220@stud.kea.dk>
2019-05-28 16:32:48 +0200

    i failed at life, and also at orderManagement i leave it you max senpai GAMBATE !

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -84,16 +84,28 @@ export default new Router({
         import("./views/admin/Movie/MovieEdit.vue")
     },
     {
-      path: "/admin/Order",
-          name: "admin-order",
+      path: "/admin/orders",
+          name: "admin-orders",
         component: () =>
       import("./views/admin/Order/Orders.vue")
     },
     {
-      path: "/admin/ScheduleManagement",
-          name: "admin-schedule",
+        path: "/admin/order",
+            name: "admin-order",
         component: () =>
-      import("./views/admin/ScheduleManagement.vue")
+        import("./views/admin/Order/Order.vue")
+    },
+    {
+        path: "/admin/orderAdd",
+            name: "admin-orderAdd",
+        component: () =>
+        import("./views/admin/Order/OrderAdd.vue")
+    },
+    {
+        path: "/admin/orderEdit",
+            name: "admin-orderEdit",
+        component: () =>
+        import("./views/admin/Order/OrderEdit.vue")
     },
     {
       path: "/admin/TheatreManagement",
--- frontend/src/views/admin/ScheduleManagement.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1>Schedule</h1>
-    </div>
-</template>
