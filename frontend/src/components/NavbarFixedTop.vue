b1ca3a331de7cc70f5499d14d7ad8551d84f3c4e
Max Campbell <maxc@maxc.in>
2019-05-31 20:51:44 +0200

    Added photos, movies and fixed nav

--- frontend/src/components/NavbarFixedTop.vue
+++ frontend/src/components/NavbarFixedTop.vue
@@ -2,35 +2,27 @@
     <div id="nav">
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top border border-dark rounded-sm">
             <a class="navbar-brand" href="/"><img src="../../../backend/src/main/resources/static/img/logo.png" style="max-height: 26px"></a>
-
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
                 <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <router-link to='/' tag='li' active-class='active' class='nav-item'><a class='nav-link' >Home</a></router-link>
+                    <router-link to='/' tag='li' active-class='active' class='nav-item'><a class='nav-link' >Home</a></router-link>
                     <router-link to="/about" tag="li" active-class="active" class="nav-item"><a class="nav-link" >About</a></router-link>
                 </ul>
-                <!-- LOGIN IN THE NAVBAR-->
                 <div class="text-center" v-if="!$store.state.user">
                     <a href="" class="btn btn-default btn-rounded" data-toggle="modal" data-target="#modalLoginForm">Log in</a>
                 </div>
-                <!-- END OF LOG IN FORM -->
-                <!-- SIGN UP IN THE NAVBAR-->
                 <div class="text-center" v-if="!$store.state.user">
                     <a href="" class="btn btn-default btn-rounded" data-toggle="modal" data-target="#modalSignupForm">Sign up</a>
                 </div>
-                <div class="dropdown dropleft" v-if="$store.state.user">
-                    <button class="btn btn-default btn-rounded dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        {{$store.state.user.firstName}}
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <router-link to="/user/order" class="dropdown-item" >Orders</router-link>
-                        <a href="" v-on:click="logout" class="dropdown-item">Logout</a>
-                    </div>
+                <div class="text-center" v-if="$store.state.user">
+                    <router-link to="/user/order" class="btn btn-default btn-rounded" >Orders</router-link>
+                    <a href="" v-on:click="logout" class="btn btn-default btn-rounded">Logout</a>
                 </div>
                 <div class="text-center" v-if='$store.state.user && $store.state.user.roles.find((role)=>role.id == 2)'>
                     <router-link to="/admin" class="btn btn-default btn-rounded" >Admin Area</router-link>
                 </div>
-
-                <!-- END OF SIGN UP FORM-->
             </div>
         </nav>
     </div>
